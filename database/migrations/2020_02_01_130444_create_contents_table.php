6b474d4957f32f698d048446cbbb40201592418f
biel40 <bielet40@gmail.com>
2020-02-01 23:59:28 +0100

    added contents table migration

---
+++ database/migrations/2020_02_01_130444_create_contents_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contents', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string('title');
+            $table->date('release_date');
+            $table->text('description')->nullable();
+            $table->string('imgUrl')->nullable();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contents');
+    }
+}
